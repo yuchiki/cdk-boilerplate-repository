31a2e5afff08ae4a7d179feec705bff611e14a81
yuchiki <14884013+yuchiki@users.noreply.github.com>
2025-03-05 22:43:22 +0900

    delete s3

--- lib/replacehere-stack.ts
+++ lib/replacehere-stack.ts
@@ -1,13 +1,9 @@
-import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib'
-import * as s3 from 'aws-cdk-lib/aws-s3'
+import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
 export class ReplacehereStack extends Stack {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
-
-    new s3.Bucket(this, 'Bucket', {
-      removalPolicy: RemovalPolicy.DESTROY,
-    })
   }
 }
