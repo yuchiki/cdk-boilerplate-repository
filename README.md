bcc3fd9003e3f80f349b2a0479be4ccbacf6d82a
yuchiki <14884013+yuchiki@users.noreply.github.com>
2025-03-05 22:17:16 +0900

    snapshot

--- README.md
+++ README.md
@@ -24,3 +24,4 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 - `npx cdk deploy` deploy this stack to your default AWS account/region
 - `npx cdk diff` compare deployed stack with current state
 - `npx cdk synth` emits the synthesized CloudFormation template
+- `npm run snapshot:update` snapshot test 用の snapshot test を update する
--- test/replacehere.test.ts
+++ test/replacehere.test.ts
@@ -1,2 +1,12 @@
-test('SQS Queue Created', () => {
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+
+import { ReplacehereStack } from '../lib/replacehere-stack'
+
+test('snapshot test', () => {
+  const app = new cdk.App()
+  const stack = new ReplacehereStack(app, 'Replacehere')
+  const template = Template.fromStack(stack).toJSON()
+
+  expect(template).toMatchSnapshot()
 })
